57afa4761a65c1429824097f3defedda03bd6451
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-07-14 16:58:03 -0600

    finish methods

--- test/Trie-test.js
+++ test/Trie-test.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import Node from '../lib/Node'
 import Trie from '../lib/Trie';
+import fs from 'fs';
 
 describe('TRIE', () => {
   let trie;
@@ -9,11 +10,13 @@ describe('TRIE', () => {
     trie = new Trie();
   });
 
+  describe('insert', () => {
+
   it('should have a root node that defaults to null', () => {
-    expect(trie.root).to.equal(null);
+    expect(trie.root).to.equal(new Node());
   })
 
-  it.only('should be able to take in a word', () => {
+  it('should be able to take in a word', () => {
 
     trie.insert('hello');
     expect(trie.count).to.eq(1);
@@ -22,5 +25,34 @@ describe('TRIE', () => {
     trie.insert('good');
     // trie.insert('great')
   })
+})
 
+
+  describe('suggest', () => {
+
+    it('should give you a suggestion based on a prefix', () => {
+
+      trie.insert('brandon');
+      trie.insert('brandlord');
+      trie.insert('brand');
+
+      // trie.suggest('bra');
+      expect(trie.suggest('bra')).to.equal(['brand', 'brandon', 'brandlord'])
+
+
+
+    })
+  })
+
+  describe('populate', () => {
+
+    it('should populate my trie with my dictionary', () => {
+      const text = "/usr/share/dict/words";
+      const dictionary = fs.readFileSync(text).toString().trim().split('\n');
+
+      trie.populate(dictionary);
+
+      expect(trie.count).to.eq(235886)
+    })
+  })
 });
